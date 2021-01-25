aeb46b3ec138400dfd57227440e061d55b332f72
Michael <mihali5@mail.ru>
2021-01-25 21:29:42 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 export function App() {
   return (
     <div className="App">
-     
+    <BrowserRouter>
+      <Route path="/guide" component={() => <h1>Byeyer's Guide</h1>}/>
+    </BrowserRouter>
     </div>
   );
 }
