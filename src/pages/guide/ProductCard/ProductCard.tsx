355bfbb1e24d8a08c019a32e50dc4819a72280d7
Michael <mihali5@mail.ru>
2021-01-30 15:28:55 +0300

    add Releases component

--- src/pages/guide/ProductCard/ProductCard.tsx
+++ src/pages/guide/ProductCard/ProductCard.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import s from "./productCard.module.css";
 import { Product } from "./Product/Product";
+import { Releases } from "./Releases/Releases";
 
 
 export const ProductCard = () => {
   return (
     <section className={s.releaseCard}>
       <Product/>
+      <Releases/>
     </section>
       
   )
---
+++ src/pages/guide/ProductCard/Releases/Releases.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import s from "./productCard.module.css";
+
+export const Releases = () => {
+  return (
+    <section>
+      
+    </section>
+  )
+}
