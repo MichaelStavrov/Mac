88b9ed783121a4a5a4bce8358a2a8a769fbf7a7e
Michael <mihali5@mail.ru>
2021-01-30 19:25:30 +0300

    add recent releases

--- src/pages/guide/ProductCard/ReleaseProgressWrapper/ReleaseProgressWrapper.tsx
+++ src/pages/guide/ProductCard/ReleaseProgressWrapper/ReleaseProgressWrapper.tsx
@@ -4,17 +4,39 @@ import { ProgressLine } from "../ProgressLine/ProgressLine";
 
 type ReleaseProgressWrapperProps = {
   date: string
+  days: number
 }
 
-export const ReleaseProgressWrapper = ({date}: ReleaseProgressWrapperProps) => {
+export const ReleaseProgressWrapper = ({date, days}: ReleaseProgressWrapperProps) => {
+
+
+  const months: any = {
+    "01": "Jan",
+    "02": "Feb",
+    "03": "Mar",
+    "04": "Apr",
+    "05": "May",
+    "06": "Jun",
+    "07": "Jul",
+    "08": "Aug",
+    "09": "Sep",
+    "10": "Oct",
+    "11": "Nov",
+    "12": "Dec" 
+  }
+  const year: string = date.slice(date.length - 4)
+  const month: string = date.slice(date.length - 7, date.length - 5)
+
+
+
   return (
     <div className={s.releaseProgressWrapper}>
       <div className={s.left}>
-        <div className={s.date}>{date}</div>
+        <div className={s.date}>{months[month]} {year}</div>
         <ProgressLine/>
       </div>
       <div className={s.right}>
-        <div className={s.days}>81</div>
+        <div className={s.days}>{days}</div>
       </div>
       
     </div>
--- src/pages/guide/ProductCard/Releases/Releases.tsx
+++ src/pages/guide/ProductCard/Releases/Releases.tsx
@@ -3,8 +3,32 @@ import s from "./releases.module.css";
 import { ReleaseProgressWrapper } from "../ReleaseProgressWrapper/ReleaseProgressWrapper";
 
 
-const date = ["Nov 2020", "Mar 2020", "Jul 2019", "Oct 2018", "Jun 2017", "Mar 2015", "Apr 2014"]
+const dates = ["10.11.2020", "18.03.2020", "09.07.2019", "30.10.2018", "05.06.2017", "09.03.2015", "29.04.2014"]
 
+function parseDate(str: any): any {
+  const mdy = str.split('.'); 
+  return new Date(mdy[2], mdy[1] - 1, mdy[0]);
+}
+
+function getDateDiff(arrayDates: string[]) {
+  const days = []
+  for (let i = 0; i < arrayDates.length - 1; i++) {
+    let diff = Math.round((parseDate(arrayDates[i]) - parseDate(arrayDates[i + 1])) / (1000*60*60*24));
+    days.push(diff)
+  }
+  return days;
+}
+
+function createArrayDateWithDiff(dates: string[], days: number[]) {
+  const result = [];
+  const datesSlice = dates.slice(1)
+  for (let i = 0; i < datesSlice.length; i++) {
+    result.push({date: datesSlice[i], days: days[i]})
+  }
+  return result;
+}
+
+const datesWithDiff = createArrayDateWithDiff(dates, getDateDiff(dates));
 
 
 
@@ -16,7 +40,7 @@ export const Releases = () => {
           <h3 className={s.title}>Recent releases</h3>
         </div>
         <div className={s.rightPart}>
-          {date.map(date => <ReleaseProgressWrapper date={date}/>)}
+          {datesWithDiff.slice(0, 6).map(obj => <ReleaseProgressWrapper date={obj.date} days={obj.days} key={Math.random()}/>)}
         </div>
       </div>
      
