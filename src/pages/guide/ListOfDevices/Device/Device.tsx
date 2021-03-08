b87799668eba7ded95a38cf12514ae0beda96bf2
Michael <mihali5@mail.ru>
2021-03-08 17:41:07 +0300

    fix status Device component

--- src/pages/guide/ListOfDevices/Device/Device.tsx
+++ src/pages/guide/ListOfDevices/Device/Device.tsx
@@ -1,7 +1,17 @@
 // import React from 'react';
 import s from './device.module.css';
-import { IMacFamily } from "../../../../types/macs"
+import { IMacFamily, IMacModelDict, IMacModelId } from "../../../../types/macs"
 import { imgs } from "../../utils/getImg";
+import { useSelector } from 'react-redux';
+import { IRootState } from '../../../../store';
+import { ProductColorStatus, ProductStatus } from '../../../../types/productStatus';
+import { getDatesMeta } from '../../ProductCard/utils/getDatesMeta';
+import { getDaysSinceLastRelease } from '../../ProductCard/utils/getDaysSinceLastRelease';
+import { getProductBuyStatus, getStatus } from '../../ProductCard/utils/getStatus';
+import { releasesDateInfo } from '../../ProductCard/utils/releasesDateInfo';
+import { getDates } from '../../utils/getDates';
+import { getMacFamilyIds } from '../../utils/getMacFamilyIds';
+import { macsArrayToDict } from '../../utils/macsArrayToDict';
 
 
 type DeviceProps = {
@@ -9,9 +19,19 @@ type DeviceProps = {
   onChangeFamily: (macFamily: IMacFamily) => void
 }
 
-const productBuyStatus="Buy Now" 
+// const productBuyStatus="Buy Now" 
 
 export const Device = ({ mac, onChangeFamily }: DeviceProps) => {
+  const macs = useSelector((state: IRootState) => state.macs.entities);
+  
+  const macModelDict: IMacModelDict = macsArrayToDict(macs);
+  const macModelIds: IMacModelId[] = getMacFamilyIds(macModelDict, mac);  
+  const dates: Date[] = getDates(macModelIds, macModelDict).slice(0, 7);
+  const daysSinceLastRelease = getDaysSinceLastRelease(dates[0]);
+  const arrayDatesWithDiff = releasesDateInfo(dates);
+  const { max } = getDatesMeta(arrayDatesWithDiff);
+  const status = arrayDatesWithDiff.length > 0 ? getStatus(daysSinceLastRelease, max) : ProductStatus.neutral;
+  const background = arrayDatesWithDiff.length > 0 ? ProductColorStatus[status] : ProductColorStatus[ProductStatus.neutral];
   
   return (
     <li className={s.device} >
@@ -22,7 +42,7 @@ export const Device = ({ mac, onChangeFamily }: DeviceProps) => {
         <h4 className={s.title} >{mac}</h4>
       </div>
       <div className={s.status}>
-        <div className={s.productBuyStatus}>{productBuyStatus}</div>
+        <div className={s.productBuyStatus}  style={{ background, border: `1px solid ${background}` }}>{getProductBuyStatus(status)}</div>
       </div>
     </li>
   )
