40b6a66f10e1d49d7065f677881d74a6da00cff2
Michael <mihali5@mail.ru>
2021-03-11 15:17:50 +0300

    add  tabbar

--- src/pages/guide/Guide.tsx
+++ src/pages/guide/Guide.tsx
@@ -5,6 +5,11 @@ import { ProductCard } from "./ProductCard/ProductCard";
 import { ListOfDevices } from "./ListOfDevices//ListOfDevices"
 import {IRootState} from "../../store";
 
+import iconMacbook from '../../img/tabBar/icon-13macbook.png'
+import iconIPhone from '../../img/tabBar/icon-iphone.png'
+import iconMusic from '../../img/tabBar/icon-music.png'
+import iconWatch from '../../img/tabBar/icon-watch.png'
+
 export const Guide = () => {
   const status = useSelector((state: IRootState) => state.macs.loading);
 
@@ -15,6 +20,24 @@ export const Guide = () => {
 
   return (
     <section className={s.guide}>
+      <ul className={s.tabBar}>
+        <li className={s.item}>
+          <img src={iconIPhone} className={s.image} alt='icon-iphone'/>
+          IPhone/IPad
+        </li>
+        <li className={s.item} >
+        <img src={iconMacbook} className={s.image} alt='icon-macbook'/>
+          Macs
+        </li>
+        <li className={s.item} >
+        <img src={iconMusic} className={s.image} alt='icon-music'/>
+          Music
+        </li>
+        <li className={s.item} >
+        <img src={iconWatch} className={s.image} alt='icon-watch'/>
+          Watch/Other
+        </li>
+      </ul>
       <ListOfDevices />
       <ProductCard />
     </section>
